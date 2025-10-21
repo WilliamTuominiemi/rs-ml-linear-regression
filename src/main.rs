05cf3617ab1425319591d093cafed5b2d12ac7db
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-10-21 22:21:36 +0300

    read dataset

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use csv::ReaderBuilder;
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+
+#[derive(Debug, Deserialize)]
+struct Datapoint {
+    tv: f64,        // feature
+    radio: f64,     // feature
+    newspaper: f64, // feature
+    sales: f64,     // label
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let dataponts = read_data()?;
+
+    println!("{:?}", dataponts);
+
+    Ok(())
+}
+
+fn read_data() -> Result<Vec<Datapoint>, Box<dyn Error>> {
+    let mut datapoints = vec![];
+
+    let mut rdr = csv::Reader::from_path("advertising.csv")?;
+    for result in rdr.deserialize() {
+        let datapoint: Datapoint = result?;
+        datapoints.push(datapoint);
+    }
+
+    Ok(datapoints)
 }

2eff11adfd362eeb80fd1cc8791f7cec1e78c905
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-10-22 22:12:20 +0300

    mean squared error

--- src/linearRegression.rs
+++ src/linearRegression.rs
@@ -1,3 +1,5 @@
+use std::result;
+
 pub struct LinearRegression {
     features: Vec<Vec<f64>>,
     labels: Vec<f64>,
@@ -18,5 +20,23 @@ impl LinearRegression {
             features: biased_features,
             labels,
         }
+    }
+
+    fn mean_squared_error(actual_outputs: Vec<f64>, predicted_outputs: Vec<f64>) -> f64 {
+        let n = actual_outputs.len();
+        if n != predicted_outputs.len() {
+            panic!("Actual and predicted outputs need to be same length");
+        }
+
+        let mut result = 0.0;
+
+        for i in 0..n {
+            let difference = actual_outputs[i] - predicted_outputs[i];
+            let squared_difference = difference.powf(2.0);
+            result += squared_difference
+        }
+
+        let mean = result / n as f64;
+        mean
     }
 }
